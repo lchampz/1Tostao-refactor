583c247e914d92b1a7423c0599e33ccd2f342b34
VictorLonchamps <victor.longchamps@pro22.com.br>
2022-03-10 20:36:35 -0300

    feat: adicionando o map dos cards

--- 1tostao/src/components/molecules/CategoryCarroussel/index.js
+++ 1tostao/src/components/molecules/CategoryCarroussel/index.js
@@ -1,17 +1,21 @@
+
 import React from 'react'
-
 import Card from '../../atoms/Card'
-
 import { Wrapper } from './styled.js'
+
+const images = require("../../../request/mock/imgs.json")
+
+
+const renderCards = images.map((img, i) => {
+    return(<Card className={'Card'} url={img.url} key={i}>{i}</Card>)
+})
+
 
 const CategoryCarroussel = () => {
 
     return(
         <Wrapper>
-            <Card className={'Card'} bgImg={'https://sportshub.cbsistatic.com/i/r/2019/11/07/04a0f1ab-3a63-408f-aefa-2eebc50ddc65/thumbnail/770x433/b28dcbd21e1c80b8a282bdba6b9340a3/13fb012e-7aff-e911-80cd-fa7ca2e6058b-original.jpg'}>teste</Card>
-            <Card className={'Card'}>teste</Card>
-            <Card className={'Card'}>teste</Card>
-            <Card className={'Card'}>teste</Card>
+            {renderCards}
         </Wrapper>
     )
 }
