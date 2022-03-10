bb3babd3d62c2c805564d3b059ecbb27f5584b82
oFandangos <raphael.feitosa11@gmail.com>
2022-03-10 20:26:47 -0300

    menu almost finished

---
+++ 1tostao/src/components/atoms/LinkMenu/Link.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './LinkStyle.css'
+
+const Link = () => {
+    return(
+        <div>
+            <h2 className="link">1Tostão</h2>
+            <p className="config">Configurações</p>
+        </div>
+    );
+}
+
+export default Link;
