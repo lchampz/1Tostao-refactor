49f4b30ed4d3b0dc078ea12044547377e6dacbe5
oFandangos <raphael.feitosa11@gmail.com>
2022-03-10 19:14:47 -0300

    commit de teste

--- 1tostao/src/index.js
+++ 1tostao/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Menu from './components/organims/Menu/Menu.js'
+import Header from './components/molecules/Header/Header.js'
 
 import './reset.css'
 
 
 ReactDOM.render(
+  
   <Menu />,
   document.getElementById('root')
 );
