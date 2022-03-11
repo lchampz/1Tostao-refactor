28a6103fa46daf4cc9c4f6f614bec94ff0fbfd2f
VictorLonchamps <victor.longchamps@pro22.com.br>
2022-03-11 20:46:18 -0300

    feat: adicionando lib react-carousel e modelando o carousel

--- 1tostao/src/components/molecules/CategoryCarroussel/index.js
+++ 1tostao/src/components/molecules/CategoryCarroussel/index.js
@@ -1,21 +1,50 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../../atoms/Card'
-import { Wrapper } from './styled.js'
+import { Wrapper, Arrow } from './styled.js'
 
+import Carousel, { consts } from 'react-elastic-carousel'
 const images = require("../../../request/mock/imgs.json")
 
-
-const renderCards = images.map((img, i) => {
-    return(<Card className={'Card'} url={img.url} key={i}>{i}</Card>)
-})
-
-
 const CategoryCarroussel = () => {
+    
+    const arrow = ({ type, onClick, isEdge }) => {
+        const pointer = type === consts.PREV ? '<' : '>'
+
+        return(
+            <Arrow onClick={onClick} disabled={isEdge}>
+                {pointer}
+            </Arrow>
+        )
+    }
+
+    const breakpoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+        { width: 850, itemsToShow: 4, itemsToScroll: 1 },
+    ]
+
+    const renderCards = images.map((img, i) => {
+        return(
+            <Card key={i} className={'Card'} url={img.url} >{img.id}</Card>     
+        )  
+    })
 
     return(
         <Wrapper>
-            {renderCards}
+            <Carousel renderArrow={arrow}
+                easing="cubic-bezier(1,.15,.55,1.54)"
+                tiltEasing="cubic-bezier(0.110, 1, 1.000, 0.210)"
+                transitionMs={700}
+                breakPoints={breakpoints}
+                isRTL={true} 
+                focusOnSelect={true} 
+                itemsToScroll={1} 
+                itemsToShow={4}
+                enableAutoPlay autoPlaySpeed={2000}
+            >
+                {renderCards}
+            </Carousel>
         </Wrapper>
     )
 }
--- 1tostao/src/components/molecules/CategoryCarroussel/styled.js
+++ 1tostao/src/components/molecules/CategoryCarroussel/styled.js
@@ -4,5 +4,19 @@ export const Wrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+    margin-left: 20%;
+    margin-right: 20%;
 `
-   
+
+export const Arrow = styled.div`
+    font-size: 30px;
+    color: black;
+    font-family: 'Poppins', sans-serif;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    font-weight: bold;
+`
