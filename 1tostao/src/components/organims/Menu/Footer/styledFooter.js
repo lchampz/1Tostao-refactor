922acb3f6a14e4d99a057b624c428480703a57e5
oFandangos <raphael.feitosa11@gmail.com>
2022-03-12 15:34:08 -0300

    feat: bug fix footer

--- 1tostao/src/components/organims/Menu/Footer/styledFooter.js
+++ 1tostao/src/components/organims/Menu/Footer/styledFooter.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const FooterStyled = styled.footer`
     margin:0;
     position:absolute;
-    background-color:#747474;
+    background-color:#16151A;
     height:395px;
     width:100%;
     margin-top:20px;
