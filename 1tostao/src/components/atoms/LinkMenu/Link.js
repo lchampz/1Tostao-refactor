bb5461740b050171c5ab8e5e4732aac1297dd6fd
oFandangos <raphael.feitosa11@gmail.com>
2022-03-22 18:32:00 -0300

    feat: menu fixed

--- 1tostao/src/components/atoms/LinkMenu/Link.js
+++ 1tostao/src/components/atoms/LinkMenu/Link.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Links } from './styledLink.js'
+import logo from '../../../assets/img/logo-white.png'
+import './styled.css'
 
 const Link = ({children, float, color, margin, margintop, font, size, fontfamily}) => {
     return(
         <>
-            <Links linkMargin={margin="20px"}>
-                1Tostão
+            <Links>
+                <img src={logo} className="logo" />
             </Links>
 
             <Links linkSize={size="16px"} 
             linkLogo={float="right"} 
-            linkTop={margintop="20px"}
-            linkMargin={margin="none"}>
+            linkTop={margintop="40px"}
+            linkMargin={margin="20px"}>
                 Configurações
             </Links>
         </>
