1d88c780ebe997dc9b6386dc201a6822ab9d3f21
VictorLonchamps <victor.longchamps@pro22.com.br>
2022-03-22 11:01:49 -0300

    feat: adding aboutUs section on home

--- 1tostao/src/components/atoms/Border/styled.js
+++ 1tostao/src/components/atoms/Border/styled.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 export const BorderText = styled.div`
-    border: ${({border}) => (border ? border : "1px solid #ffffff")};
+    border: ${({ border, size, color}) => (border ? border : (size && color ? `${size} solid ${color}` :`1px solid white`))};
     width: ${({borderWidth}) => (borderWidth ? borderWidth : "90%")};
     margin-top: ${({topBorder}) => (topBorder ? topBorder: "20px")};
     margin-bottom: ${({bottomBorder}) => (bottomBorder ? bottomBorder: "20px")};
+
 `
