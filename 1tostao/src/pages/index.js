ebfc4ed3e8cea80204d78a4613a0924f4ba1fa52
murilo-simoes <murilorsimoes@gmail.com>
2022-03-25 19:39:26 -0300

    fix: archives

--- 1tostao/src/pages/index.js
+++ 1tostao/src/pages/index.js
@@ -2,8 +2,5 @@ export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as AboutUs } from './AboutUs';
 export { default as Register } from './Register';
-<<<<<<< HEAD
 export { default as Servicos } from './Servicos';
-=======
 export { default as FAQ } from './FAQ';
->>>>>>> 6325fb0289ca006a0b2f7105fbae2a8ddab0f1fc
--- 1tostao/src/routes.js
+++ 1tostao/src/routes.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-<<<<<<< HEAD
-import { AboutUs, Home, Login, Register, Servicos } from './pages'
-=======
-import { AboutUs, Home, Login, Register, FAQ } from './pages'
->>>>>>> 6325fb0289ca006a0b2f7105fbae2a8ddab0f1fc
+import { AboutUs, Home, Login, Register, Servicos, FAQ  } from './pages'
+
 
 const Rotas = () => {
 
@@ -14,21 +11,14 @@ const Rotas = () => {
         { path: '/register', name: 'Register', element: <Register />},
         { path: '/about', name: 'About', element: <AboutUs />},
         { path: '/faq', name: 'FAQ', element: <FAQ />},
+        { path: '/servicos', name: 'Servicos', element: <Servicos />},
     ]
 
     return(
         <Routes>
-<<<<<<< HEAD
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/about" element={<AboutUs/>} />
-            <Route path="/servicos" element={<Servicos/>} />
-=======
             {routes.map((routes, i) => (
                 <Route key={i} path={routes.path} element={routes.element}/>
             ))}
->>>>>>> 6325fb0289ca006a0b2f7105fbae2a8ddab0f1fc
         </Routes>
     )
 }
