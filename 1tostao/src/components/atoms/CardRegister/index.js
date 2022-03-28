4947269f93ba084977a89e271a145e6d1376cab9
VictorLonchamps <victor.longchamps@pro22.com.br>
2022-03-28 17:31:37 -0300

    feat: adding a mini-state-manager at register

--- 1tostao/src/components/atoms/CardRegister/index.js
+++ 1tostao/src/components/atoms/CardRegister/index.js
@@ -1,4 +1,4 @@
-import react from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Container, Card, Input, Label, Button, variavel } from './styled.js'
 import {Title} from '../Text/styles/text_style.js';
@@ -6,7 +6,9 @@ import {Title} from '../Text/styles/text_style.js';
 
 const CardRegister = ({ children, color, fontSize, textAlign, marginBottom, transition }) => {
     const navigate = useNavigate();
-    return(
+    const [page, setPage] = useState(1)
+
+    return page === 1 ?(
         <>
         <Container>
             <Card>
@@ -21,14 +23,28 @@ const CardRegister = ({ children, color, fontSize, textAlign, marginBottom, tran
                     <Input placeholder="Digite seu CPF" />
                     <Label>Senha</Label>
                     <Input placeholder="Digite sua senha" />
-                <Button onClick={() => navigate(`/`)}  transitionButton={transition="0.2s ease-in-out"}>
+                <Button onClick={() => setPage(2)}  transitionButton={transition="0.2s ease-in-out"}>
                     <Label colorLabel={color="white"} sizeLabel={fontSize="20px"} alignLabel={textAlign="center"} >Avançar</Label>
                 </Button>
                 <Label>Já tem uma conta? Entrar</Label>
             </Card>
         </Container>        
         </>
-    );
+    ) : (<><Container>
+        <Card>
+            <Title colorText={color="black"} sizeText={fontSize="50px"} bottomText={marginBottom="90px"}>
+                Cadastro parte 2
+            </Title>
+                <Label>teste</Label>
+                <Input placeholder="teste" />
+                <Label>teste</Label>
+                <Input placeholder="teste" />
+            <Button onClick={() => setPage(1)}  transitionButton={transition="0.2s ease-in-out"}>
+                <Label colorLabel={color="white"} sizeLabel={fontSize="20px"} alignLabel={textAlign="center"} >Voltar!</Label>
+            </Button>
+            <Label>Já tem uma conta? Entrar</Label>
+        </Card>
+    </Container>  </>);
 };
 
 export default CardRegister;
