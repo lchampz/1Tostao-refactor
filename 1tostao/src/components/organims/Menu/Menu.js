23dd811f3d4deab75bc6bbca6fac4d4cdc99b8f8
murilo-simoes <murilorsimoes@gmail.com>
2022-03-30 18:20:13 -0300

    alteração

--- 1tostao/src/components/organims/Menu/Menu.js
+++ 1tostao/src/components/organims/Menu/Menu.js
@@ -1,59 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Botao from '../../atoms/BotaoMenu/Botao'
 import { useNavigate } from 'react-router-dom'
-import { Header, Pages, Buttons, Container} from './styledMenu.js'
+import { Header, Pages, Buttons, Container, HamburguerMenu, MobileMenu, PagesMenu} from './styledMenu.js'
 import logo from '../../../assets/img/logo-white.png'
 import { Links } from '../../atoms/LinkMenu/styledLink.js'
 import LinkMenu from '../../atoms/LinkMenu/Link.js'
 import { useTheme} from '../../../request/hooks/Theme'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons'
 
 const Menu = ({padding}) => {
+    const [classMenu, setClassMenu] = useState();
     const navigate = useNavigate();
     const { theme, setTheme, themes } = useTheme()
     
     const changeTheme = () => {
         setTheme(theme.name === 'white' ? themes[1] : themes[0])
     }
+    
 
-
-
+    
+    
+    
 document.addEventListener("scroll", () => {
-    const content = document.getElementById("section");
-    let scrolled = document.scrollingElement.scrollTop;
+        const content = document.getElementById("section");
+        let scrolled = document.scrollingElement.scrollTop;
     let position = content.offsetTop;
 
-     if(scrolled > position + 75){
+    if(scrolled > position + 75){
         content.style.backgroundColor=`rgba(54,54,54)`;
-        content.style.boxShadow=`rgba(0, 0, 0, 0.24) 0px 3px 8px`
+        content.style.boxShadow=`rgba(0, 0, 0, 0) 0px 0px 0px`
         content.style.transition=`all 0.2s ease-in`;
-        }else if(scrolled > position - 75){
-            content.style.backgroundColor=`rgba(255,255,255, 0)`;
-            content.style.boxShadow=`rgba(0, 0, 0, 0) 0px 0px 0px`
-            content.style.transition=`all 0.2s ease-in`;
-        }
+    }else if(scrolled > position - 75){
+        content.style.backgroundColor=`rgba(255,255,255, 0)`;
+        content.style.boxShadow=`rgba(0, 0, 0, 0) 0px 0px 0px`
+        content.style.transition=`all 0.2s ease-in`;
+    }
 });
 
+const handleMenu = () =>{
+    const openMenu = document.getElementById("mobile");
+    const display = document.getElementById("hamburguer");  
 
-
-    return(
+    openMenu.addEventListener = ('click', () => {
+        if(display.classList === "off"){
+            setClassMenu("active")
+            display.style.display=`block`;
+            display.classList.add(classMenu);
+        }else if(display.classList === "active"){
+            setClassMenu("off")
+            display.style.display=`none`;
+            display.classList.add(classMenu);
+        }
+    })
+}
+    
+return(
+    <>
         <Container id="section">
-            <Header>
                 <Links onClick={() => navigate(`/`)} linkTop={padding}>
-                     <img  src={logo} className="logo" />
+                     <img  src={logo} className="logo"/>
                 </Links>
-                <Pages onClick={() => navigate(`/`)}>Home</Pages>
-                <Pages onClick={() => navigate(`/servicos`)}>Serviços</Pages>
-                <Pages onClick={() => navigate(`/about`)}>Sobre nós</Pages>
-                <Pages onClick={changeTheme}>Tema</Pages>
-            </Header>
+ 
+                <Header id="links">
+                        <Pages onClick={() => navigate(`/`)}>Home</Pages>
+                        <Pages onClick={() => navigate(`/servicos`)}>Serviços</Pages>
+                        <Pages onClick={() => navigate(`/about`)}>Sobre nós</Pages>
+                        <Pages onClick={changeTheme}>Tema</Pages>
+                </Header>
             <Buttons>
                  <Botao click={() => navigate(`/login`)}>Login</Botao>
                  <Botao click={() => navigate(`/register`)}>Registrar</Botao>
             </Buttons>
-                
-            
+            <HamburguerMenu id="mobile" onClick={handleMenu} >
+                <FontAwesomeIcon className="icon" icon={faBars} />
+            </HamburguerMenu>
         </Container>
+            <MobileMenu id="hamburguer"className='off'>
+                    <PagesMenu  onClick={() => navigate(`/`)}>Home</PagesMenu>
+                    <PagesMenu onClick={() => navigate(`/servicos`)}>Serviços</PagesMenu>
+                    <PagesMenu onClick={() => navigate(`/about`)}>Sobre nós</PagesMenu>
+                    <PagesMenu onClick={changeTheme}>Tema</PagesMenu>
+            </MobileMenu>
+        </>
     );
+
 }
 
 export default Menu;
--- 1tostao/src/components/organims/Menu/styledMenu.js
+++ 1tostao/src/components/organims/Menu/styledMenu.js
@@ -3,18 +3,21 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width:100%;
-    height:10vh;
+    height:75px;
     background-color:rgb(255, 255, 255, 0);
     position:fixed;
     z-index: 999;
     display:flex;
+    align-items: center;
 
 `;
 
-export const Header = styled.ul`
+export const Header = styled.div`
     width: 165vh;
     align-items: center;
     display: flex;
+    position: relative;
+
 `
 
 export const Pages = styled.li`
@@ -22,11 +25,15 @@ export const Pages = styled.li`
     margin-right: 40px;
     margin-left: 15px;
     color:#fff;
+    display: block;
     transition: all 0.1s ease-in;
     list-style-type: none;
     &:hover{
         color: #24D39A;
         cursor: pointer;
+    }
+    @media(max-width:840px){
+        display: none;
     }
 
 `
@@ -36,4 +43,66 @@ export const Buttons = styled.div`
     justify-content: end;
     align-items: center;
     display:flex;
+
+    @media(max-width:840px){
+        display: none;
+    }
+`
+
+export const HamburguerMenu = styled.div`
+    .icon{
+        display: none;
+        margin-right:30px;
+        font-size: 1.8rem;
+        color:#fff;
+        float: right;
+        transition: all 0.1s ease-in;
+
+        
+
+        &:hover{
+            color:#24D39A;
+            cursor: pointer;
+        }
+        @media(max-width:840px){
+            display: block;
+        }
+    }
+`
+
+export const MobileMenu = styled.div`
+    display: none;
+    position: fixed;
+    width: 100%;
+    height: 14rem;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    padding-top:35px;
+    transition: all 0.1s ease-in;
+    background-color: rgba(54,54,54);
+    z-index: 997;
+    
+    @media(max-width: 840px){
+        
+    }
+    
+    `
+export const PagesMenu = styled.li`
+    margin-top: 15px;
+    padding-bottom: 5px;
+    font-size: 0.9rem;
+    color:#fff;
+
+    transition: all 0.1s ease-in;
+    list-style-type: none;
+
+    &:first-child{
+        padding-top: 25px;
+    }
+
+    &:hover{
+        color: #24D39A;
+        cursor: pointer;
+    }
 `
