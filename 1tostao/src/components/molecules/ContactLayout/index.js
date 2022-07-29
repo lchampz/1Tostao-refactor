59767e053136dc6a30959b4b1c4b62c92bcc3bba
murilo-simoes <murilorsimoes@gmail.com>
2022-07-29 19:46:40 -0300

    feat: send email

--- 1tostao/src/components/molecules/ContactLayout/index.js
+++ 1tostao/src/components/molecules/ContactLayout/index.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import {Wrapper, Send, Card, Forms,Botao, ButtonDiv,  FormMessage, Message, Label} from './styled'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_sjivoln', 'template_k0l3cdu', form.current, 'IZ9MwYC4-q08MezXL')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+    };
     return ( 
         <>
             <Wrapper>
                 <Send>Envie-nos uma mensagem!</Send>
                 <Card>
-                    <Forms>
-                        <Label>Assunto</Label>
-                        <FormMessage type="text"></FormMessage>
-                        <Label>Mensagem</Label>
-                        <Message></Message>
-                        <ButtonDiv>
-                            <Botao>Enviar</Botao>
-                        </ButtonDiv>
-                    </Forms>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <Forms>
+                                <Label>Assunto</Label>
+                                <FormMessage type="text" name="user_name"></FormMessage>
+                                <Label>Mensagem</Label>
+                                <Message name="message"></Message>
+                                <ButtonDiv>
+                                    <Botao>Enviar</Botao>
+                                </ButtonDiv>
+                        </Forms>
+                    </form>
                 </Card>
             </Wrapper>
         </>
