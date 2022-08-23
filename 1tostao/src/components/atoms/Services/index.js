c2735986831cedb848905f2203f3d3191736e40f
murilo-simoes <murilorsimoes@gmail.com>
2022-08-23 19:33:36 -0300

    servicos pasta

---
+++ 1tostao/src/components/atoms/Services/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Services = () => {
+
+    return ( 
+            <div>
+              <p>Olá</p>
+            </div>
+     );
+}
+ 
+export default Services;
--- 1tostao/src/pages/Servicos.js
+++ 1tostao/src/pages/Servicos.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ServiceFilter from '../components/atoms/ServiceFilter'
+import Services from '../components/atoms/Services'
 import Footer from '../components/organims/Footer/Footer.js'
 const Servicos = () => {
 
     return(
         <>
             <ServiceFilter/>
+            <Services/>
             <Footer/>
         </>   
     )
