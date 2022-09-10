4bf3b22053d53396d2ccaee9148eed980f9ae274
murilo-simoes <murilorsimoes@gmail.com>
2022-09-10 20:11:04 -0300

    code pretty

--- 1tostao/src/components/atoms/Services/index.js
+++ 1tostao/src/components/atoms/Services/index.js
@@ -1,50 +1,72 @@
-import React from 'react';
-import {Container, ServiceWrapper, Service,Price, ServicePrice, InfoService, ClientImg, ClientName, ServiceTitle, ServiceImage} from './styled'
+import React from "react";
+import {
+  Container,
+  ServiceWrapper,
+  Service,
+  Price,
+  ServicePrice,
+  InfoService,
+  ClientImg,
+  ClientName,
+  ServiceTitle,
+  ServiceImage,
+} from "./styled";
 import service1 from "../../../assets/img/service1.png";
 import service2 from "../../../assets/img/service2.png";
 import service3 from "../../../assets/img/service3.png";
-import profilePic from './../../../assets/img/profile.png'
-import { useService } from '../../../request/hooks/Services.js';
-import {useUserAuth} from '../../../request/hooks/Auth'
+import profilePic from "./../../../assets/img/profile.png";
+import { useService } from "../../../request/hooks/Services.js";
+import { useUserAuth } from "../../../request/hooks/Auth";
 
 const Services = () => {
-    const {service } = useService();
-    const {user } = useUserAuth();
- 
-    console.log(service)
-    return ( 
-        <Container>
-            <ServiceWrapper>
-            {service?.map(item => {
-                return(
-            <Service key={item.id}>
-                   
-                            <ServiceTitle>{item.nome}</ServiceTitle>
-                            <div style={{display:"flex", flexDirection:"row", width:"250px", justifyContent:"flex-end"}}>
-                                <ServicePrice>
-                                    <Price>R${item.preco}</Price>
-                                </ServicePrice>
-                            </div>
-                            <ServiceImage src={service1}/>
-        
-                            <InfoService>
-                                <div style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
-                                    <ClientImg src={user && user.photoURL ? user && user.photoURL : profilePic}/>
-                                    <ClientName>{item.autor}</ClientName>
-                                </div>
-                                <p id="avaliacao">3,6</p>
-                            </InfoService>
-                    
-                </Service>
+  const { service } = useService();
+  const { user } = useUserAuth();
 
-                )
-            }
-                 )}  
-                 
-               
-            </ServiceWrapper>
-        </Container>
-     );
-}
- 
+  console.log(service);
+  return (
+    <Container>
+      <ServiceWrapper>
+        {service?.map((item) => {
+          return (
+            <Service key={item.id}>
+              <ServiceTitle>{item.nome}</ServiceTitle>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "250px",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <ServicePrice>
+                  <Price>R${item.preco}</Price>
+                </ServicePrice>
+              </div>
+              <ServiceImage src={service1} />
+
+              <InfoService>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <ClientImg
+                    src={
+                      user && user.photoURL ? user && user.photoURL : profilePic
+                    }
+                  />
+                  <ClientName>{item.autor}</ClientName>
+                </div>
+                <p id="avaliacao">3,6</p>
+              </InfoService>
+            </Service>
+          );
+        })}
+      </ServiceWrapper>
+    </Container>
+  );
+};
+
 export default Services;
--- 1tostao/src/components/atoms/Services/styled.js
+++ 1tostao/src/components/atoms/Services/styled.js
@@ -1,106 +1,98 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    background-color: #403B3B;
-    padding-bottom: 2rem;
-    padding-top: 1rem;
-`
+  display: flex;
+  justify-content: center;
+  background-color: #403b3b;
+  padding-bottom: 2rem;
+  padding-top: 1rem;
+`;
 
 export const ServiceWrapper = styled.div`
-    width: 80%;
-    color: #eee;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-    #avaliacao{
-        font-size: 0.8rem;
-        font-weight: bold;
-    }
-    `
+  width: 80%;
+  color: #eee;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  #avaliacao {
+    font-size: 0.8rem;
+    font-weight: bold;
+  }
+`;
 
 export const Service = styled.div`
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    border-radius: 25px;
-    width: 265px;
-    margin-right:1.5rem;
-    margin-top:3rem;
-    border: 3px solid #d0d0d0;
-    height: 250px;
-    transition: 50ms all ease-in;
-    background-color: #484848;
-    box-shadow:rgba(0, 0, 0, 0.2)  8px 8px 10px;
-    
-    @media screen and (max-width:1280px){
-        margin-right:4rem;
-    }
-    @media screen and (max-width:1000px){
-        margin-right:1.5rem;
-    }
-   
-    &:hover{
-        border: 3px solid rgb(1, 220, 138);
-        box-shadow: rgb(1, 220, 138, 0.1)  5px 5px 10px;
-      
-    }
-   
-`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 25px;
+  width: 265px;
+  margin-right: 1.5rem;
+  margin-top: 3rem;
+  border: 3px solid #d0d0d0;
+  height: 250px;
+  transition: 50ms all ease-in;
+  background-color: #484848;
+  box-shadow: rgba(0, 0, 0, 0.2) 8px 8px 10px;
+
+  @media screen and (max-width: 1280px) {
+    margin-right: 4rem;
+  }
+  @media screen and (max-width: 1000px) {
+    margin-right: 1.5rem;
+  }
+
+  &:hover {
+    border: 3px solid rgb(1, 220, 138);
+    box-shadow: rgb(1, 220, 138, 0.1) 5px 5px 10px;
+  }
+`;
 
 export const InfoService = styled.div`
-    margin-top: 0.4rem;
-    width: 92%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-`
+  margin-top: 0.4rem;
+  width: 92%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 export const ServiceTitle = styled.h1`
-    margin-bottom: 0.5rem;
-    text-align: center;
-    width: 250px;
-`
+  margin-bottom: 0.5rem;
+  text-align: center;
+  width: 250px;
+`;
 
 export const ServiceImage = styled.img`
-    width: 250px;
-    border-radius: 7px;
-    height: 150px;
-    transition: 0.1s all ease-in;
-    box-shadow:rgba(0, 0, 0, 0.4)  5px 5px 10px;
-  
-`
+  width: 250px;
+  border-radius: 7px;
+  height: 150px;
+  transition: 0.1s all ease-in;
+  box-shadow: rgba(0, 0, 0, 0.4) 5px 5px 10px;
+`;
 
 export const ClientImg = styled.img`
-    margin-right: 0.5rem;
-    width: 35px;
-    border-radius: 50px;
-  
-`
+  margin-right: 0.5rem;
+  width: 35px;
+  border-radius: 50px;
+`;
 
 export const ClientName = styled.h3`
-    font-size: 0.8rem;
-`
+  font-size: 0.8rem;
+`;
 
 export const ServicePrice = styled.div`
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color:rgb(1, 160, 138);
-    width: 65px;
-    font-size: 14px;
-    height: 30px;
-    border-top-right-radius: 7px;
-    
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(1, 160, 138);
+  width: 65px;
+  font-size: 14px;
+  height: 30px;
+  border-top-right-radius: 7px;
+`;
 
-`
-
-export const Price = styled.p`
-
-`
+export const Price = styled.p``;
